4a8945b29af7bdde72482b6736acffd083244ea1
Magdalena Ojrzynska <magdalena.ojrzynska@gmail.com>
2024-05-13 20:48:13 +0200

    latest changes

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -52,6 +52,7 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+
   class Product {
     constructor(id, data) {
       const thisProduct = this;
@@ -60,7 +61,10 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product:', thisProduct);
     }
@@ -86,14 +90,26 @@
 
     }
 
+    getElements() {
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
+
+
     initAccordion() {
       const thisProduct = this;
 
-      /* find the clickable trigger (the element that should react to clicking) */
+      /* find the clickable trigger (the element that should react to clicking) 
       const clickableTrigger = this.element.querySelector(select.menuProduct.clickable);
 
       /* Start: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function (event) {
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
         /* prevent default action for event */
         event.preventDefault();
 
@@ -111,12 +127,80 @@
       });
 
     }
+
+    initOrderForm() {
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function () {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      console.log();
+    }
+
+    processOrder() {
+      const thisProduct = this;
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          //check if there is param with a name of paramId in formData and if it includes optionId
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if (!option.default == true) {
+              // add option price to price variable
+              let price = thisProduct.data.price + option.priceElem;
+              console.log(price);
+
+            } else {
+              // check if the option is default
+              if (option.default == true) {
+                // reduce price variable 
+                let price = thisProduct.data.price - option.priceElem;
+                console.log(price);
+              }
+            }
+          }
+
+          // update calculated price in the HTML
+          thisProduct.priceElem.innerHTML = price;
+        }
+      }
+    }
   }
   const app = {
 
     initMenu: function () {
-      //const testProduct = new Product();
-      //console.log('testProduct:' , testProduct);
+      // const testProduct = new Product();
+      // console.log('testProduct:' , testProduct);
 
       const thisApp = this;
 
@@ -149,3 +233,4 @@
 
   app.init();
 }
+
